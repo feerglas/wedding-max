3dc841de7dd87782931a697017c1ab036b57dab9
Yves Tscherry <info@feerglas.com>
2021-03-27 13:30:19 +0100

    add button component

---
+++ src/components/Button.vue
@@ -0,0 +1,28 @@
+<template>
+  <button
+    class="button"
+    :disabled="disable"
+  >{{ text }}</button>
+</template>
+
+<script>
+export default {
+  name: 'Button',
+  props: {
+    text: String,
+    disable: Boolean,
+  },
+};
+</script>
+
+<style scoped lang="scss">
+
+.button {
+  color: red;
+}
+
+.button:disabled {
+  opacity: .5;
+}
+
+</style>
