5ed681a2777d54fc27e58eb5db80404f0ac3a159
Yves Tscherry <info@feerglas.com>
2021-03-27 12:42:26 +0100

    add template for logged in state

---
+++ src/templates/LoggedIn.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="main-wrapper">
+    <div class="main-content">
+      <Nav />
+      <router-view />
+    </div>
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Nav from '@/components/Nav.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Nav,
+  },
+};
+</script>
