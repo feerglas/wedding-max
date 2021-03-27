a578cc044d05b9d2238688d8055176ccd6667675
Yves Tscherry <info@feerglas.com>
2021-03-27 12:42:43 +0100

    remove nav

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,8 @@
 <template>
   <div id="app">
-    <Nav />
-    <router-view/>
+    <router-view />
   </div>
 </template>
-
-<script>
-import Nav from '@/components/Nav.vue';
-
-export default {
-  name: 'App',
-  components: {
-    Nav,
-  },
-};
-</script>
 
 <style lang="scss">
 #app {
