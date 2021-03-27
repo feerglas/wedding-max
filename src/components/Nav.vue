129c537e6e0126c44c5aa74ff1e2d0c8d47951fb
Yves Tscherry <info@feerglas.com>
2021-03-27 12:29:22 +0100

    add nav component

---
+++ src/components/Nav.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="nav">
+    <router-link to="/login">Login</router-link> |
+    <router-link to="/">Detail</router-link> |
+    <router-link to="/registration">Registration</router-link> |
+    <router-link to="/confirmation">Confirmation</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Nav',
+};
+</script>
+
+<style scoped lang="scss">
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+
+</style>
