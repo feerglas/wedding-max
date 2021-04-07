9566153d518c1aeabab1512c85a7edff7e132d0c
Yves Tscherry <info@feerglas.com>
2021-04-07 20:01:48 +0200

    integrate svg logos

--- src/components/BookingTeasers.vue
+++ src/components/BookingTeasers.vue
@@ -1,13 +1,22 @@
 <template>
     <ul class="list list--booking-teasers">
-        <li><a href="https://www.airbnb.ch/" target="_blank">Airbnb</a></li>
-        <li><a href="https://www.booking.com/" target="_blank">Booking.com</a></li>
-        <li><a href="https://www.tripadvisor.ch/" target="_blank">Tripadvisor</a></li>
+        <li><a href="https://www.airbnb.ch/" target="_blank"><LogoAirbnb /></a></li>
+        <li><a href="https://www.booking.com/" target="_blank"><LogoBooking /></a></li>
+        <li><a href="https://www.tripadvisor.ch/" target="_blank"><LogoTripadvisor /></a></li>
     </ul>
 </template>
 
 <script>
+import LogoAirbnb from '../../public/img/svg/logo_airbnb.svg';
+import LogoBooking from '../../public/img/svg/logo_booking.svg';
+import LogoTripadvisor from '../../public/img/svg/logo_tripadvisor.svg';
+
 export default {
+  components: {
+    LogoAirbnb,
+    LogoBooking,
+    LogoTripadvisor,
+  },
   name: 'BookingTeasers',
   props: {},
 };
@@ -37,6 +46,19 @@ export default {
         width: pxToRem(1);
         content: '';
         background: rgba($colorGroom, .2);
+    }
+
+    .list--booking-teasers li a {
+      color: $colorGroomsMan;
+    }
+
+    .list--booking-teasers li a:hover {
+      color: $colorGroom;
+    }
+
+    .list--booking-teasers li svg {
+      height: 1.5rem;
+      fill: currentColor;
     }
 
 </style>
