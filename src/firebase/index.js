70aa421dda51587930e2837b6dc12c7ef05ca7b6
Yves Tscherry <info@feerglas.com>
2021-04-07 19:28:29 +0200

    init and export firebase instance for registrations

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,14 +1,24 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
+import 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
   authDomain: 'max-wedding.firebaseapp.com',
+  projectId: 'max-wedding',
 };
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
+// auth instance
 const auth = firebase.auth();
 
-export default auth;
+// db instance and collection
+const db = firebase.firestore();
+const registrations = db.collection('registrations');
+
+export {
+  auth,
+  registrations,
+};
