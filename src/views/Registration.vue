5d8d4f5bc050da464d96f5476729665101b90bf9
Yves Tscherry <info@feerglas.com>
2021-04-08 19:01:53 +0200

    add registration components to registration page

--- src/views/Registration.vue
+++ src/views/Registration.vue
@@ -1,23 +1,28 @@
 <template>
   <div>
-    <p>Registration Page</p>
-    <router-link to="/">Link back to detail</router-link><br>
-    <router-link to="/confirmation">Link to confirmation</router-link>
-
-    <button @click="submit">submit</button>
+    <router-link to="/">
+      <Logo />
+    </router-link>
 
     <Registration1 />
+    <Registration2 />
+
+    <button @click="submit">submit</button>
   </div>
 </template>
 
 <script>
 import { addRegistration } from '@/firebase/registrations';
 import Registration1 from '@/components/Registration1.vue';
+import Registration2 from '@/components/Registration2.vue';
+import Logo from '@/components/Logo.vue';
 
 export default {
   name: 'Registration',
   components: {
     Registration1,
+    Registration2,
+    Logo,
   },
   methods: {
     async submit() {
