7ec51a4f3cbee6aaa78ff3e8d1ae6c3d7020265c
Yves Tscherry <info@feerglas.com>
2021-04-09 18:58:22 +0200

    add checkboxes and radios for rough selection

--- src/components/Registration2.vue
+++ src/components/Registration2.vue
@@ -1,11 +1,78 @@
 <template>
-  <div>Reg 2</div>
+  <div>
+    <h1>Ich nehme gerne teil</h1>
+
+    <form>
+
+      <fieldset>
+
+        <!-- Get together -->
+        <Checkbox
+          value="gettogether"
+          label="Get together"
+          sublabel="Freitag, 20.08.2021"
+          v-model="checkboxSelectionEventGettogether"
+        />
+
+        <!-- Wedding -->
+        <Checkbox
+          value="wedding"
+          label="Hochzeitsfeier"
+          sublabel="Samstag, 20.08.2021"
+          v-model="checkboxSelectionEventWedding"
+        />
+
+        GETTOGETHER: {{checkboxSelectionEventGettogether}}
+        WEDDING: {{checkboxSelectionEventWedding}}
+
+        <br><br>
+        <Radiogroup
+          :radios="radioData"
+          v-model="radioSelectionWedding"
+          :selected="radioData.items[0].value"
+        />
+        SELECTED: {{radioSelectionWedding}}
+
+      </fieldset>
+
+    </form>
+
+  </div>
 </template>
 
 <script>
+import Checkbox from '@/components/Checkbox.vue';
+import Radiogroup from '@/components/Radiogroup.vue';
+
 export default {
   name: 'Registration2',
+  components: {
+    Checkbox,
+    Radiogroup,
+  },
   props: {},
+  data() {
+    return {
+      radioSelectionWedding: '',
+      checkboxSelectionEventGettogether: false,
+      checkboxSelectionEventWedding: false,
+      radioData: {
+        id: 'weddingPersons',
+        items: [
+          {
+            value: '1',
+            id: '1person',
+            label: '1 Person',
+          },
+          {
+            value: '2',
+            id: '2person',
+            label: '2 Personen',
+          },
+        ],
+      },
+    };
+  },
 };
 </script>
 
