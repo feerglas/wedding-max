6f24c2d70ac61929847db5469007f715f81d7d40
Yves Tscherry <info@feerglas.com>
2021-04-09 18:57:24 +0200

    add component for checkbox

---
+++ src/components/Checkbox.vue
@@ -0,0 +1,47 @@
+<template>
+
+  <div :class="sublabel ? 'has-sublabel' : ''">
+
+    <label>
+
+      <input
+        type="checkbox"
+        :value="value"
+        :checked="modelValue"
+        @change="updateInput"
+      >
+
+      {{label}}
+      <span v-if="sublabel">{{sublabel}}</span>
+
+    </label>
+
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'Checkbox',
+  model: {
+    prop: 'modelValue',
+    event: 'change',
+  },
+  props: {
+    value: String,
+    modelValue: {
+      default: false,
+    },
+    label: String,
+    sublabel: String,
+  },
+  methods: {
+    updateInput(event) {
+      this.$emit('change', event.target.checked);
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss">
+</style>
