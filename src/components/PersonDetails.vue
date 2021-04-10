86e72d4d8524981cf9380bc985772c0e1b9fdc77
Yves Tscherry <info@feerglas.com>
2021-04-10 12:21:13 +0200

    add component for person details

---
+++ src/components/PersonDetails.vue
@@ -0,0 +1,52 @@
+<template>
+
+  <div>
+
+    <h2>Person 1</h2>
+
+    <Input
+      type="text"
+      icon="user"
+      id="name1"
+      label="Dein Vorname & Nachname"
+      autocomplete="username"
+      :required="true"
+      error-empty="Deinen Namen muss ich wissen..."
+      :validation="$v.name"
+    />
+
+  </div>
+
+</template>
+
+<script>
+import { validationMixin } from 'vuelidate';
+import { required } from 'vuelidate/lib/validators';
+import Input from '@/components/Input.vue';
+
+export default {
+  name: 'PersonDetails',
+  mixins: [validationMixin],
+  components: {
+    Input,
+  },
+  computed: {
+    name: {
+      get() {
+        return this.$store.getters.name1;
+      },
+      set(newValue) {
+        this.$store.commit('setName1', newValue);
+      },
+    },
+  },
+  validations: {
+    name: {
+      required,
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss">
+</style>
