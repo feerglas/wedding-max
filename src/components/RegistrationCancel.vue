197f4e2b035769ba43a6bf91d01c153481db334f
Yves Tscherry <info@feerglas.com>
2021-04-11 16:14:36 +0200

    add contact data

--- src/components/RegistrationCancel.vue
+++ src/components/RegistrationCancel.vue
@@ -7,18 +7,34 @@
 
     <p>Melde dich bei uns, falls du es dir doch noch anders überlegst.</p>
 
-    <ContactItems />
+    <ContactItems :items="contactItemsData" />
 
   </div>
 </template>
 
 <script>
 import ContactItems from '@/components/ContactItems.vue';
+import contactData from '@/data/contactItems';
 
 export default {
   name: 'RegistrationCancel',
   components: {
     ContactItems,
+  },
+  data() {
+    return {
+      contactItemsData: {
+        sections: [
+          {
+            title: 'Melde dich bei uns, falls du es dir doch noch anders überlegst.',
+            items: [
+              contactData.sarah,
+              contactData.max,
+            ],
+          },
+        ],
+      },
+    };
   },
 };
 </script>
