f5a5e83bb519f9cdddbd69803337e47c32151a4a
Yves Tscherry <info@feerglas.com>
2021-04-11 16:14:22 +0200

    add contact data to separate file

---
+++ src/data/contactItems.js
@@ -0,0 +1,22 @@
+export default {
+  lenna: {
+    name: 'Lenna Friedrich',
+    phone: '+41 79 819 42 99',
+    eMail: '',
+  },
+  niklas: {
+    name: 'Niklas Bathke',
+    phone: '+49 176 66613678',
+    eMail: '',
+  },
+  sarah: {
+    name: 'Sarah Pröckl',
+    phone: '+41 76 611 77 61',
+    eMail: '',
+  },
+  max: {
+    name: 'Maximilian Hammes',
+    phone: '+41 76 611 61 14',
+    eMail: '',
+  },
+};
