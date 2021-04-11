42f8d543a0967163207ab3da85602e9eacfaa5e7
Yves Tscherry <info@feerglas.com>
2021-04-11 16:25:24 +0200

    remove text

--- src/components/RegistrationCancel.vue
+++ src/components/RegistrationCancel.vue
@@ -5,8 +5,6 @@
 
     <p>Schade, dass du nicht dabei sein kannst.</p>
 
-    <p>Melde dich bei uns, falls du es dir doch noch anders überlegst.</p>
-
     <ContactItems :items="contactItemsData" />
 
   </div>
