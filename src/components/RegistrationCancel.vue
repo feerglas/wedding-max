f54e3ae171461b1bc66352aa1996e4876f4cdf0b
Yves Tscherry <info@feerglas.com>
2021-04-11 15:58:11 +0200

    add contact items

--- src/components/RegistrationCancel.vue
+++ src/components/RegistrationCancel.vue
@@ -1,16 +1,30 @@
 <template>
-  <div>
-      cancel
+  <div class="step2">
+
+    <h1 class="title-1">Du verpasst was</h1>
+
+    <p>Schade, dass du nicht dabei sein kannst.</p>
+
+    <p>Melde dich bei uns, falls du es dir doch noch anders überlegst.</p>
+
+    <ContactItems />
 
   </div>
 </template>
 
 <script>
+import ContactItems from '@/components/ContactItems.vue';
+
 export default {
   name: 'RegistrationCancel',
+  components: {
+    ContactItems,
+  },
 };
 </script>
 
 <style scoped lang="scss">
-
+.step1 {
+  margin-bottom: pxToRem($interModuleSpacingMobile);
+}
 </style>
