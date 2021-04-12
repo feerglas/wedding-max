357aa75418afcb377edc1d48cb99694307da7f26
Yves Tscherry <info@feerglas.com>
2021-04-12 16:42:08 +0200

    use correct key for mail

--- src/data/contactItems.js
+++ src/data/contactItems.js
@@ -2,21 +2,21 @@ export default {
   lenna: {
     name: 'Lenna Friedrich',
     phone: '+41 79 819 42 99',
-    eMail: 'lenna.friedrich@web.de',
+    mail: 'lenna.friedrich@web.de',
   },
   niklas: {
     name: 'Niklas Bathke',
     phone: '+49 176 66613678',
-    eMail: 'niklasbathke@googlemail.com',
+    mail: 'niklasbathke@googlemail.com',
   },
   sarah: {
     name: 'Sarah Pröckl',
     phone: '+41 76 611 77 61',
-    eMail: 's.proeckl@googlemail.com',
+    mail: 's.proeckl@googlemail.com',
   },
   max: {
     name: 'Maximilian Hammes',
     phone: '+41 76 611 61 14',
-    eMail: 'mxhammes@googlemail.com',
+    mail: 'mxhammes@googlemail.com',
   },
 };
