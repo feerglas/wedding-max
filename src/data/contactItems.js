fe94bd229ac86abfa52a398ea741cba452e46d83
Yves Tscherry <info@feerglas.com>
2021-04-12 16:41:01 +0200

    add email addresses

--- src/data/contactItems.js
+++ src/data/contactItems.js
@@ -2,21 +2,21 @@ export default {
   lenna: {
     name: 'Lenna Friedrich',
     phone: '+41 79 819 42 99',
-    eMail: '',
+    eMail: 'lenna.friedrich@web.de',
   },
   niklas: {
     name: 'Niklas Bathke',
     phone: '+49 176 66613678',
-    eMail: '',
+    eMail: 'niklasbathke@googlemail.com',
   },
   sarah: {
     name: 'Sarah Pröckl',
     phone: '+41 76 611 77 61',
-    eMail: '',
+    eMail: 's.proeckl@googlemail.com',
   },
   max: {
     name: 'Maximilian Hammes',
     phone: '+41 76 611 61 14',
-    eMail: '',
+    eMail: 'mxhammes@googlemail.com',
   },
 };
