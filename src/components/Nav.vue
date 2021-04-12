15a6cf9c2e62f1021d005e4381066a89571f1b4e
Yves Tscherry <info@feerglas.com>
2021-04-12 16:44:35 +0200

    fix nav top margin

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -85,7 +85,7 @@ a {
       flex-direction: column;
       flex: 0 0 pxToRem(250);
       width: pxToRem(250);
-      margin-top: pxToRem(0);
+      margin-top: pxToRem($interElementSpacing);
       margin-left: pxToRem(-275);
       padding-left: pxToRem(25);
   }
